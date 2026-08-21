9d4c6795f92d9bb34857fca52fb449ee1e96e81e
Carter Irish <2023c.irish@gmail.com>
2026-08-21 04:17:19 -0400

    Fix: Enhance error handling in duel command for user identification

--- src/commands/duel.ts
+++ src/commands/duel.ts
@@ -1,52 +1,65 @@
 // src/commands/duel.ts
 import {
-	ApplicationCommandOptionType,
-	InteractionResponseType,
-	type APIChatInputApplicationCommandInteraction,
-	type APIApplicationCommandInteractionDataUserOption,
+    ApplicationCommandOptionType,
+    InteractionResponseType,
+    type APIChatInputApplicationCommandInteraction,
+    type APIApplicationCommandInteractionDataUserOption,
+    MessageFlags,
+    APIInteractionResponse,
 } from 'discord-api-types/v10';
 import type { Command } from './types';
 
 const randInt = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
 
+const errorReply = (content: string): APIInteractionResponse => ({
+	type: InteractionResponseType.ChannelMessageWithSource,
+	data: { content, flags: MessageFlags.Ephemeral },
+});
+
 export const duel: Command = {
-	definition: {
-		name: 'duel',
-		description: 'Shoot it out with someone',
-		options: [
-			{
-				type: ApplicationCommandOptionType.User,
-				name: 'target',
-				description: 'Who you want to fight',
-				required: true,
-			},
-		],
-	},
-	handler: async (interaction) => {
-		const i = interaction as APIChatInputApplicationCommandInteraction;
+    definition: {
+        name: 'duel',
+        description: 'Shoot it out with someone',
+        options: [
+            {
+                type: ApplicationCommandOptionType.User,
+                name: 'target',
+                description: 'Who you want to fight',
+                required: true,
+            },
+        ],
+    },
+    handler: async (interaction) => {
+        const i = interaction as APIChatInputApplicationCommandInteraction;
 
-		const challenger = i.member?.user ?? i.user!;
+        const challenger = i.member?.user ?? i.user;
+        if (!challenger) {
+            return errorReply('Could not identify who invoked this command.');
+        }
 
-		const option = i.data.options?.find(
-			(o): o is APIApplicationCommandInteractionDataUserOption =>
-				o.name === 'target' && o.type === ApplicationCommandOptionType.User,
-		);
-		const target = i.data.resolved?.users?.[option!.value]!;
+        const option = i.data.options?.find(
+            (o): o is APIApplicationCommandInteractionDataUserOption =>
+                o.name === 'target' && o.type === ApplicationCommandOptionType.User,
+        );
+        const target = option && i.data.resolved?.users?.[option.value];
+        if (!target) {
+            return errorReply('Could not find that user.');
+        }
 
-		const winner = Math.random() < 0.5 ? challenger : target;
+        const winner = Math.random() < 0.5 ? challenger : target;
 
-		const content =
-			`<@${challenger.id}> decided to fight <@${target.id}>. ` +
-			`<@${challenger.id}> has ${randInt(1, 100)} bullets and ${randInt(1, 100)}% accuracity. ` +
-			`<@${target.id}> has ${randInt(1, 100)} bullets and ${randInt(1, 100)}% accuracity. ` +
-			`shoots and shoots get shooted until... <@${winner.id}> wins!`;
+        const content =
+            `<@${challenger.id}> decided to fight <@${target.id}>. ` +
+            `<@${challenger.id}> has ${randInt(1, 100)} bullets and ${randInt(1, 100)}% accuracity. ` +
+            `<@${target.id}> has ${randInt(1, 100)} bullets and ${randInt(1, 100)}% accuracity. ` +
+            `shoots and shoots get shooted until... <@${winner.id}> wins!`;
 
-		return {
-			type: InteractionResponseType.ChannelMessageWithSource,
-			data: {
-				content,
-				allowed_mentions: { parse: [], users: [] },
-			},
-		};
-	},
+        return {
+            type: InteractionResponseType.ChannelMessageWithSource,
+            data: {
+                content,
+                allowed_mentions: { parse: [], users: [] },
+            },
+        };
+    },
 };
