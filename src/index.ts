089303a4ed98adcc15068387719942f259fb5bd7
Carter Irish <2023c.irish@gmail.com>
2026-08-21 02:26:45 -0400

    Feat: Add discord-interactions dependency and implement interaction handling in the worker

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,31 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { verifyKey} from 'discord-interactions';
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Hello World!");
-	},
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		if (request.method === 'POST') {
+			return new Response('Kingbot is running!');
+		}
+
+		const signature = request.headers.get('X-Signature-Ed25519');
+		const timestamp = request.headers.get('X-Signature-Timestamp');
+		const body = await request.text();
+		if(signature === null || timestamp === null) {
+			return new Response('Missing signature or timestamp', { status: 401 });
+		}
+		const verifiedKey = await verifyKey(body, signature, timestamp, env.DISCORD_PUBLIC_KEY);
+
+		const valid = signature && timestamp && verifiedKey;
+		if (!valid) {
+			return new Response('Invalid request', { status: 401 });
+		}
+
+		const interaction = JSON.parse(body);
+		console.log('Interaction:', JSON.stringify(interaction,null,2));
+
+		if (interaction.type === 1) {
+			return Response.json({ type: 1 });
+		}
+
+		return new Response('Unhandled interaction type', { status: 400 });
+	}
 } satisfies ExportedHandler<Env>;
