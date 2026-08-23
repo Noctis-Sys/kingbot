02414650b355a5b96735fd3610471c158fc02d1a
Carter Irish <2023c.irish@gmail.com>
2026-08-23 00:03:46 -0400

    Feat: Update 'sens' command to include detailed sensitivity settings for additional characters

--- src/commands/sens.ts
+++ src/commands/sens.ts
@@ -8,8 +8,20 @@ import { errorReply } from '../util/errorHandling';
 import { getStringOption } from '../util/options';
 
 const SENS_DATA: Record<string, string> = {
+    all: 'All Heros - 262 Horizontal 212 Vertical. Different for some characters, use /sens [hero name here] (remove [ ]) for other sens',
     magik: 'Magik (Controller): 295 Horizontal 256 Vertical',
     bucky: 'Bucky (Mouse & Keyboard): Horizontal - 1.1: Vertical - 1.1',
+    daredevil: 'Daredevil (Controller): Horizontal 440 Vertical 400',
+    deadpool: 'Deadpool (Mouse & Keyboard): horizontal: 1.2 - vertical: 1.2',
+    elsa: 'Elsa (Mouse & Keyboard): Horizontal and Vertical Sensitivity 2.30',
+    hela: 'Hela (Controller): Horizontal - 154 Vertical - 114',
+    luna: 'Luna (Mouse & Keyboard): Horizontal - 0.95 Vertical - 0.95',
+    magneto: 'Magneto (Mouse & Keyboard): Horizontal - 1.2 Vertical - 1.2',
+    psylocke: 'Psylocke (Controller): Horizontal - 256 Vertical - 216',
+    punisher: 'Punisher (Controller): Horizontal - 256 Vertical - 216',
+    spiderman: 'Spiderman (Controller): 261 Horizontal 301 Vertical',
+    wintersoldier: 'Winter Soldier (Mouse & Keyboard): Horizontal - 1.1: Vertical - 1.1',
+    wolverine: 'Wolverine (Mouse & Keyboard): Horizontal: 2.0 Vertical 2.0',
 };
 
 export const sens: Command = {
@@ -32,6 +44,54 @@ export const sens: Command = {
 						name: 'Bucky',
 						value: 'bucky',
 					},
+					{
+						name: 'Daredevil',
+						value: 'daredevil',
+					},
+					{
+						name: 'Deadpool',
+						value: 'deadpool',
+					},
+					{
+						name: 'DP',
+						value: 'dp',
+					},
+					{
+						name: 'Elsa',
+						value: 'elsa',
+					},
+					{
+						name: 'Hela',
+						value: 'hela',
+					},
+					{
+						name: 'Luna',
+						value: 'luna',
+					},
+					{
+						name: 'Magneto',
+						value: 'magneto',
+					},
+					{
+						name: 'Psylocke',
+						value: 'psylocke',
+					},
+					{
+						name: 'Punisher',
+						value: 'punisher',
+					},
+					{
+						name: 'Spiderman',
+						value: 'spiderman',
+					},
+					{
+						name: 'Winter Soldier',
+						value: 'wintersoldier',
+					},
+					{
+						name: 'Wolverine',
+						value: 'wolverine',
+					},
 				],
 			},
 		],
@@ -45,7 +105,7 @@ export const sens: Command = {
             return {
                 type: InteractionResponseType.ChannelMessageWithSource,
                 data: {
-                    content: `All Heros - 262 Horizontal 212 Vertical. Different for some characters, use /sens [put ur hero here] (remove [ ]) for other sens`,
+                    content: SENS_DATA['all'],
                     allowed_mentions: { parse: [], users: [] },
                 }
             }
